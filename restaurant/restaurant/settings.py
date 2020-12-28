6e2c141f63a5cff1cf1909ecf3b93003d624355d
keshavrkaranth <keshavarkarantha@gmail.com>
2020-12-28 12:18:46 +0530

    added posgresrSql database

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
